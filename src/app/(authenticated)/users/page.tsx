aa005222202cf98970986545b5778299eec5d504
git-denial <danielamazia@gmail.com>
2024-07-23 10:25:39 +0700

    Fetch users page data from DB

--- src/app/(authenticated)/users/page.tsx
+++ src/app/(authenticated)/users/page.tsx
@@ -1,21 +1,26 @@
 
+import UserApi from "@/app/api/UserApi"
 import { Button } from "@/components/ui/button"
+import DataTable from "./_component/dataTable"
 
-export default function UsersPage() {
 
-  //const users = await getAllUsers()
-  const users = []
+
+
+export default async function UsersPage() {
+
+  const users = JSON.parse(JSON.stringify((await UserApi.getAll())))
 
   return (
     <>
-    <div className="flex items-center">
+      <div className="flex items-center">
 
-            <h1 className="text-lg font-semibold md:text-2xl">Users</h1>
-            </div>
-          
-          <div
-            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
-          >
+        <h1 className="text-lg font-semibold md:text-2xl">Users</h1>
+      </div>
+
+      <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1">
+
+        {
+          users.length === 0 ?
             <div className="flex flex-col items-center gap-1 text-center">
               <h3 className="text-2xl font-bold tracking-tight">
                 You have no users
@@ -25,8 +30,12 @@ export default function UsersPage() {
               </p>
               <Button className="mt-4">Add User</Button>
             </div>
-          </div>
+            :
+            <DataTable data={users}/>
+        }
+
+      </div>
     </>
- 
+
   )
 }
