a12990b7b02c7bff95434a18585e8772e0ed1753
git-denial <danielamazia@gmail.com>
2024-07-23 11:55:47 +0700

    Add alert dialog

---
+++ src/components/ui/AlertDialog.tsx
@@ -0,0 +1,46 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { Dispatch, EventHandler, MouseEventHandler, SetStateAction, useState } from "react"
+import SpinnerLoading from "./spinnerloading"
+
+export default function AlertDialogue({opened, onOpenChange, headMsg, bodyMsg, continueFunction}: {opened:boolean, onOpenChange:Dispatch<SetStateAction<boolean>>, headMsg: string, bodyMsg: string, continueFunction:VoidFunction}) {
+
+  const [loading, setLoading] = useState(false)
+
+  return (
+    
+    <AlertDialog open={loading? true : opened} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{headMsg}</AlertDialogTitle>
+          <AlertDialogDescription>
+            {bodyMsg}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          
+          <AlertDialogAction disabled={loading} onClick={async ()=>{
+            setLoading(true)
+            // await new Promise(resolve => setTimeout(resolve, 5000))
+            await continueFunction()
+            setLoading(false)
+          }}>
+            {loading? <SpinnerLoading/> : null}  Continue</AlertDialogAction>
+            
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+    
+  )
+}
