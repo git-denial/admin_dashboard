77e826dcb5387ea9abd12496cf61b185fc7707ab
git-denial <danielamazia@gmail.com>
2024-07-31 14:05:18 +0700

    Disable old API

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,12 +1,12 @@
-import prisma from "@/app/lib/prisma";
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-import { NextRequest } from "next/server";
+// import prisma from "@/lib/prisma";
+// import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+// import { NextRequest } from "next/server";
 
-export async function DELETE(request:NextRequest, context: { params: Params }) {
+// export async function DELETE(request:NextRequest, context: { params: Params }) {
     
-    let id = parseInt(context.params.id)
-    let result = await prisma.users.delete({where: {id:id}})
+//     let id = parseInt(context.params.id)
+//     let result = await prisma.users.delete({where: {id:id}})
 
-    return Response.json(result)
+//     return Response.json(result)
 
-  }
+//   }
