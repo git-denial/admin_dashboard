0019616635ae2e95cfd5765eeff33a614d13e478
git-denial <danielamazia@gmail.com>
2024-08-01 10:24:49 +0700

    Add util timeUnitInSeconds

--- src/utils/generalUtil.ts
+++ src/utils/generalUtil.ts
@@ -23,8 +23,28 @@ function removeUnknownProps(x:any, props:string[]){
     for(let xp of xprop) if(!daprops.includes(xp)) delete x[xp]
 }
 
+function timeUnitInSeconds(n: number, unit: 'minute'|'hour'|'day'|'month'|'year'|'year_with_leap_year'){
+    switch (unit) {
+        case 'minute':
+            return n * (60);
+        case 'hour':
+            return n * (60 * 60); // Multiply by 60 seconds per minute and then by 60 minutes per hour
+        case 'day':
+            return n * (60 * 60 * 24); // Multiply by 24 hours per day, 60 minutes per hour, and 60 seconds per minute
+        case 'month': // Assuming a month is approximately 30 days
+            return n * (60 * 60 * 24 * 30);
+        case 'year': // Assuming a year is approximately 365 days
+            return n * (60 * 60 * 24 * 365);
+        case 'year_with_leap_year': // Assuming a year is approximately 365.25 days (accounting for leap years)
+            return n * (60 * 60 * 24 * 365.25);
+    }
+}
+
+
+
 export default{
     nowPlusDay,
     clean,
-    removeUnknownProps
+    removeUnknownProps,
+    timeUnitInSeconds
 }
