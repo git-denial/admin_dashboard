c6674d194cb15f9cc20629792c3a24986f28bcb1
git-denial <danielamazia@gmail.com>
2024-08-03 16:17:33 +0700

    Add unit test and e2e test

---
+++ src/tests/unit_test/login.test.tsx
@@ -0,0 +1,13 @@
+import { describe, test, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Page from '@/app/(unauthenticated)/login/page';
+ 
+describe('homepage', () => {
+    it('renders', () => {
+      render(<Page />);
+  
+      expect(screen.getByText('Login')).toBeDefined()
+      expect(screen.getByText('Username')).toBeDefined()
+      expect(screen.getByText('Password')).toBeDefined()
+    });
+  });
