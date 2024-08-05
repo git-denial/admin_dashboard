58833bfe7590c969d5740dd4a87dbf38a9c6e156
git-denial <danielamazia@gmail.com>
2024-08-06 00:01:02 +0700

    DELETE

--- src/app/api/users/route.ts
+++
@@ -1,8 +0,0 @@
-// import prisma from "@/lib/prisma";
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-import { NextRequest } from "next/server";
-
-export async function GET(request:NextRequest, context: { params: Params }) {
-    
-    // return await prisma.users.findMany()
-}
