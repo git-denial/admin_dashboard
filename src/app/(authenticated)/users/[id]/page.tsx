5ee395d2370fb24c286572bd4171a0f4911e24b4
git-denial <danielamazia@gmail.com>
2026-09-18 10:31:54 +0700

    Fix bug

--- src/app/(authenticated)/users/[id]/page.tsx
+++ src/app/(authenticated)/users/[id]/page.tsx
@@ -16,7 +16,6 @@ import {
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
@@ -72,34 +71,20 @@ export default async function UserDetail({params}: {params:{id:string}}) {
                 <span className="sr-only">More</span>
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent onSelect={(e)=>e.preventDefault()} className="w-10" align="end">
-            
-            <DropdownMenuItem>
+          </DropdownMenu>
             <EditDialog
             child={<Button className="w-full text-[12px] border-primary" variant={'ghost'}>Edit</Button>}
             user={userDetail}
             />
-            </DropdownMenuItem>
-
-            <DropdownMenuItem>
             <ChangePasswordDialog 
             child={<Button className="w-full text-[12px] border-primary" variant={'ghost'}>Change password</Button>}
             user={userDetail}
             />
-            </DropdownMenuItem>
-
-            <DropdownMenuItem>
             <DeleteUserDialog
             child={<Button className="w-full text-[12px] border-primary hover:bg-destructive hover:text-white" variant={'ghost'}>Delete</Button>}
             text={{head:"Are you sure?", body:"You are about to delete this user"}}
             id={userDetail.id}            
             />
-            </DropdownMenuItem>
-            
-              
-            </DropdownMenuContent>
-          </DropdownMenu>
-
         </div>
       </CardHeader>
       <CardContent className="p-6 text-sm">
